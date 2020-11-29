50e5abc72eebb4118ab4e7ae00cd72e9a2e6defd
eurico77 <euricomagalhaes61@gmail.com>
2020-11-29 14:35:25 -0300

    create store the users

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import './registerServiceWorker';
+import './service/boot';
 import router from './router';
 import store from './store';
-import './service/boot';
 
 Vue.config.productionTip = false;
 
